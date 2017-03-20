df8140cb9e56b9de1ed9f925e59fce0fee87d37e
Serg T <sergtsaeb@gmail.com>
2017-03-20 12:13:50 -0700

    adding project files for wk 2

---
+++ TwitterClient/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TwitterClient
+//
+//  Created by Sergelenbaatar Tsogtbaatar on 3/20/17.
+//  Copyright © 2017 Serg Tsogtbaatar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+    
+
+}
+
